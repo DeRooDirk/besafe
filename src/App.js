c3e8e9b799ce13b7662b10daff2645df78857eac
ZooWie <55379205+DeRooDirk@users.noreply.github.com>
2021-03-24 09:18:17 +0100

    test version
    test version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
+import MainMiddel from './components/MainMiddel'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = ( ) =>{
+    return(
+        <div className='App'>
+            <div className = 'Left'>
+                <Header/>
+                <Footer/>
+            </div>
+            <div className = 'Right'>
+                <Main/>
+                <MainMiddel/>            
+            </div>            
+        </div>
+    )
 }
 
-export default App;
+export default App
+
+
+
---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+const Main = ({title}) => {
+    const MainTitleStyle ={    
+        color:"rgb(54, 62, 97)",
+        fontSize: "5.25rem",
+        letterSpacing:" 2px",
+        fontWeight:"400",
+        textDecoration:"underline",
+        margin:"5rem"
+    }
+    return (
+        <main style={MainTitleStyle}>
+           {title}            
+        </main>
+    )
+}
+Main.defaultProps = {
+    title : 'Safety App For Women.' 
+  
+}
+export default Main
