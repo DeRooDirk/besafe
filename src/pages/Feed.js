f178ac54132cc882a25187c2dfe9a899bbd3ebd4
Giomayra <giomayra.rosales@gmail.com>
2021-03-26 14:38:16 +0100

    add Dirk design screen 4

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {  Link } from 'react-router-dom';
+import Alert from '../components/Alert';
+
 const Feed =({children}) =>{
   return(
       <>
        <Link to="/Home">Flipperkast Flipperkast Flipperkast TO HOME PAGE </Link> 
+       <Alert></Alert>
 
       </>
   )
